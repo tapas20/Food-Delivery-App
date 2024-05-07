cb35f0fed06d68a473c0dc9e6ece227a3d40ee4c
Tapas Jyoti <tapasjyoti8327@gmail.com>
2024-05-07 21:30:01 +0530

    Episode 04 - part 01

--- app.js
+++ app.js
@@ -39,10 +39,59 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = () => {
+  return (
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <img
+        className="res-logo"
+        src="https://b.zmtcdn.com/data/pictures/chains/0/19554120/0336617c9205e4ed6ccc70026f04fbbf.jpg"
+        alt="res-logo"
+      />
+      <div className="res-des">
+        <h3 className="brand-name">Meghana Foods</h3>
+        <h4 className="cuisine">Biriyani, South Indian</h4>
+        <h4 className="rating">4.4 stars</h4>
+        <h4 className="time">Arrived in ): 38 minutes</h4>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
